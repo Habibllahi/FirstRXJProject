0cba593bb04ee26b5649160bbc0341a86bbd1819
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-04-24 12:01:23 +0100

    creation of observables in three ways

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,31 @@
 import io.reactivex.Observable;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        var disposable = Observable.just(1,2,3,45,6,7,8).subscribe(System.out::println);
+       createObservableWithJust();
+       createObservableFromIterable();
+       createObservableUsingCreate();
+    }
+    public static void createObservableWithJust(){
+         var observable = Observable.just(1,2,3,4,5,6,7,8);
+         observable.subscribe(System.out::println);
+    }
+
+    public static void createObservableFromIterable(){
+        var list = Arrays.asList(9,10,11,12,13,14);
+        var observable = Observable.fromIterable(list);
+        observable.subscribe(System.out::println);
+    }
+
+    public static void createObservableUsingCreate(){
+        var observable = Observable.create(emitter -> {
+            emitter.onNext(15);
+            emitter.onNext(16);
+            emitter.onNext(17);
+            emitter.onComplete();
+        });
+        observable.subscribe(System.out::println, error->System.out.println(error.getLocalizedMessage()),()->System.out.println("completed"));
     }
 }
